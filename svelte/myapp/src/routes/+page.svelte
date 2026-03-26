4e628d530e9832c9f843ec37d1400ed62a26504c
sebas <225sebas@gmail.com>
2026-03-25 22:14:38 -0500

    tailwind and svelte framework setup

---
+++ svelte/myapp/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  function greet() {
+    alert("hola");
+  }
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button onclick={greet}>Click Me</button>
+
+
+<style lang="postcss">
+  @reference "tailwindcss";
+
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+  /* your styles go here */
+</style>
